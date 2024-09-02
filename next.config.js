a23ccc2fd99c66ddf1546804a60e9b055a248146
eriveltosilva <eriveltoclenio@gmail.com>
2024-09-02 18:36:32 +0100

    refactor: change deprecated next image config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
-    domains: ['storage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '**',
+      },
+    ],
   },
-};
+}
